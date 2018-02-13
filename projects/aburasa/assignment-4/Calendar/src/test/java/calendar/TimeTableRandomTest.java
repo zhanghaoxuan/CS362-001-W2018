e0aeb6bbc2c06c83f5475082aab6138e24a4b642
Ali Aburas <aburasa@nondalton.eecs.oregonstate.edu>
2018-02-12 19:10:41 -0800

    adding assignment-4 folder

---
+++ projects/aburasa/assignment-4/Calendar/src/test/java/calendar/TimeTableRandomTest.java
@@ -0,0 +1,31 @@
+package calendar;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for TimeTable class.
+ */
+
+public class TimeTableRandomTest {
+	
+    /**
+     * Generate Random Tests that tests TimeTable Class.
+     */
+	 @Test
+	  public void radnomtest()  throws Throwable  {
+		 
+		 
+		 
+	 }
+
+
+	
+}
